b41c8a933cf46c97093d5e441cae9c2553c63c14
Laurynas Buinauskas <laubui@ktu.lt>
2023-01-12 22:26:08 +0200

    Initial code for the controllers

---
+++ Sorting.Api.Homework/src/Sorting.Api.Homework.WebApi/Controllers/SortingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sorting.Api.Homework.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SortingController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> OrderAndSave([FromBody] string numbers)
+        {
+            return Ok("Not implemented");
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> LoadLatestFile()
+        {
+            return Ok("Not implemented");
+        }
+    }
+}
