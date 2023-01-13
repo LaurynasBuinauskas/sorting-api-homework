c1c1e3bfc9a5d86cc4f6b1a740c8060e5f32e49b
Laurynas Buinauskas <laubui@ktu.lt>
2023-01-13 10:09:36 +0200

    Sorting algorithms, unit, and speed tests for them.

---
+++ Sorting.Api.Homework/src/Sorting.Api.Homework.WebApi/Algorithms/ISortAlgorithm.cs
@@ -0,0 +1,7 @@
+﻿namespace Sorting.Api.Homework.WebApi.Algorithms
+{
+    public interface ISortAlgorithm
+    {
+        void Sort(List<int> numbers, string algorithm);
+    }
+}
---
+++ Sorting.Api.Homework/tests/Sorting.Api.Homework.WebApi.Tests.Unit/Algorithms/SortAlgorithmTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Sorting.Api.Homework.WebApi.Tests.Unit.Algorithms
+{
+    internal class SortAlgorithmTests
+    {
+        protected SortAlgorithm _sortAlgorithm;
+        protected List<int> _sortedNumbers;
+        protected const string InvalidAlgorithmExceptiontMessage = "Invalid sorting algorithm was chosen";
+
+        [SetUp]
+        public void Setup()
+        {
+            _sortAlgorithm = new SortAlgorithm();
+            _sortedNumbers = new List<int> { 
+                1, 3, 5, 6, 7, 8, 9, 11, 12, 14, 15, 16, 19, 20, 111 
+            };
+        }
+
+        [TestCase("BubbleSort")]
+        [TestCase("MergeSort")]
+        [TestCase("HeapSort")]
+        [TestCase("InsertionSort")]
+        public void Should_Sort_Using_BubbleSort(string algorithm)
+        {
+            //Given
+            var numbers = new List<int> {
+                8, 20, 11, 14, 1, 12, 19, 7, 3, 15, 111, 9, 6, 5, 16
+            };            
+
+            //When
+            _sortAlgorithm.Sort(numbers, algorithm);            
+
+            //Then
+            CollectionAssert.AreEqual(_sortedNumbers, numbers);
+        }
+
+        [Test]
+        public void Should_Throw_Invalid_Algorithm_Exception()
+        {
+            //Given
+            var numbers = new List<int> {
+                8, 20, 11, 14, 1, 12, 19, 7, 3, 15, 111, 9, 6, 5, 16
+            };
+
+            var algorithm = "Invalid";
+
+            //When
+            var exception = Assert.Throws<Exception>(() => _sortAlgorithm.Sort(numbers, algorithm));
+
+            //Then
+            Assert.That(exception.Message, Is.EqualTo("Invalid sorting algorithm was chosen"));
+        }        
+    }
+}
